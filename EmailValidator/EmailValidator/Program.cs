835f895d8068ce35f34a4b8f90b9ec337b6062d3
fidan <fidanji@code.edu.az>
2023-08-24 11:08:08 +0400

    email validator

---
+++ EmailValidator/EmailValidator/Program.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleApp.Validations;
+using ConsoleApp.Exceptions;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        { 
+            var emailValidator = new EmailValidator();
+
+            try
+            {
+                bool isValid = emailValidator.Validate("ali.aliyev@mail.com");
+                Console.WriteLine(isValid);
+            }
+            catch (ShortEmailException ex)
+            {
+                Console.WriteLine("ShortEmailException: " + ex.Message);
+            }
+            catch (NotAnEmailAdressException ex)
+            {
+                Console.WriteLine("NotAnEmailAdressException: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ EmailValidator/EmailValidator/Validations/EmailValidator.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleApp.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Validations
+{
+    public class EmailValidator
+    {
+        public bool Validate(string email)
+        {
+            if (email.Length < 10)
+            {
+                throw new ShortEmailException("This is not considered an email.");
+            }
+
+            if (!email.EndsWith("@mail.com"))
+            {
+                throw new NotAnEmailAdressException("This email is not ending with the way it should be.");
+            }
+            return true;
+        }
+    }
+}
